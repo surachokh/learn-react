ecb3c6e4738442b0042c8a44b5c2db46732d4039
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-10 11:27:55 +0700

    Add: Lorem and Page padding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./components/Header";
 import Contact from "./pages/Contact";
 import Education from "./pages/Education";
 import Experience from "./pages/Experience";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+
+const AppWrapper = styled.div`
+  padding: 36px 120px;
+`;
 
 export default function App() {
   const [menuList] = useState([
@@ -17,13 +22,15 @@ export default function App() {
   return (
     <>
       <Header>{menuList}</Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </AppWrapper>
     </>
   );
 }
