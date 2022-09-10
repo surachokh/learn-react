c9dead1a2072d32aac926d8c15393d4d9dd1fdd1
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-10 15:37:12 +0700

    Remove Routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./components/Header";
-import Contact from "./pages/Contact";
-import Education from "./pages/Education";
-import Experience from "./pages/Experience";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
 
 const AppWrapper = styled.div`
-  padding: 36px 120px;
+  padding: 36px 240px;
 `;
 
 export default function App() {
-  const [menuList] = useState([
-    "Profile",
-    "Education",
-    "Experience",
-    "Contact",
-  ]);
   return (
     <>
-      <Header>{menuList}</Header>
+      <Header />
       <AppWrapper>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </AppWrapper>
     </>
