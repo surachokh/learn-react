5c3ca4046a17e61f451cdedae64f854f7b4922f9
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-10 15:36:32 +0700

    Add content.

--- src/common/Content.js
+++ src/common/Content.js
@@ -1,2 +1,37 @@
 export const PersonalDetail =
-  "My name is Surachok Hemadhulin. I am 25 years old with 1.5 years of work experiences. My specialize is Frontend developments and my favorites JavaScript's library is ReactJS. I have half years of work experiences on React and 1 years of experiences on Angular2(Typescripts) Version 8.";
+  "My name is Surachok Hemadhulin. I am 25 years old with 1.5 years of work experiences. My specialize is Frontend developments and my favorites JavaScript's library is ReactJS. I have half years of experiences on React and 1 years of experiences on Angular2 (Typescripts) Version 8. For now, I am finding jobs as React Developer. I prefer WFH and Hybrid Works. Please contacts me if you are interesed. ";
+
+export const ProgrammingSkillContent = ["JavaScripts", "HTML", "CSS", "C#"];
+
+export const FrameworkSkillContent = ["React", "AngularTS", "Unity"];
+
+export const SoftSkillContent = [
+  "Agile",
+  "Hard Working",
+  "Problem Solving",
+  "Communication",
+];
+
+export const EducationDetail = [
+  "Major: Computer Engineering",
+  "Minor: Information Computer Engineering",
+  "Cumulative Grade Point Average : 2.60 ",
+];
+
+export const Work = ["KBTG", "SCB", "Skooldio"];
+
+export const KBTGDetail = [
+  "Maintenance KBond Web using AngularTS",
+  "Develop KBond Web Application",
+  "Use Agile Methodology to develop application",
+];
+
+export const SCBTechX = [
+  "Maintenance SCB Partner API",
+  "Onboarding Partner to develop API",
+];
+
+export const Skooldio = [
+  "Develop features on Skooldio Classroom Web",
+  "Maintenance Skooldio Web Application",
+];
