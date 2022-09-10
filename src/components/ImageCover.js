c0072278444e3aa168fbfb5d2e4b9bdaf6aa0521
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-10 16:19:20 +0700

    Fix: Image to adjust from props

--- src/components/ImageCover.js
+++ src/components/ImageCover.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 const Frame = styled.img`
   border-radius: 50%;
-  width: 300px;
-  height: 300px;
+  width: ${(props) => props.size ?? "240px"};
+  height: ${(props) => props.size ?? "240px"};
   object-fit: contain;
 `;
 
 export default function ImageCover(props) {
-  return <Frame src={props.children} alt={props.alt} />;
+  return <Frame src={props.children} alt={props.alt} size={props.size} />;
 }
