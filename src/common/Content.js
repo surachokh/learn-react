ea18dd8db7f60d6ff492a98101f5956a2738c896
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-10 11:33:42 +0700

    Add: Content

---
+++ src/common/Content.js
@@ -0,0 +1,2 @@
+export const PersonalDetail =
+  "My name is Surachok Hemadhulin. I am 25 years old with 1.5 years of work experiences. My specialize is Frontend developments and my favorites JavaScript's library is ReactJS. I have half years of work experiences on React and 1 years of experiences on Angular2(Typescripts) Version 8.";
